b1ef7b04ca78833c16f4b6ee2c38eca7ac4aac1e
zibloidix <zibloidix@gmail.com>
2020-01-27 10:09:16 +1100

    Extract flkTest to outside function. Add moment.js

--- public/app/res/js/flkFactory.js
+++ public/app/res/js/flkFactory.js
@@ -1,3 +1,20 @@
+function isEmptySoname(record) {
+    const error = { 
+        status: false, 
+        code: 'ZL_LIST/ZAP/Z_SL/OS_SLUCH', 
+        text: `Признак "Особый случай" при регистрации обращения за медицинской помощью. 
+               Необходимое значение: 2 – в документе, удостоверяющем личность пациента /родителя (представителя) пациента, отсутствует отчество
+              `
+    };
+    
+    if (record._pacient.ot === null || `${record._pacient.ot}`.trim().length === 0) { 
+        if (record.zl_list.zap.z_sl.os_sluch !== "2") {
+            error.status = true;
+        }
+    }
+    return error;
+}
+
 (function(){
     'use strict';
     angular
@@ -12,23 +29,6 @@
         const tests = [
             isEmptySoname
         ];
-
-        function isEmptySoname(record) {
-            const error = { 
-                status: false, 
-                code: 'ZL_LIST/ZAP/Z_SL/OS_SLUCH', 
-                text: `Признак "Особый случай" при регистрации обращения за медицинской помощью. 
-                       Необходимое значение: 2 – в документе, удостоверяющем личность пациента /родителя (представителя) пациента, отсутствует отчество
-                      `
-            };
-            
-            if (record._pacient.ot === null || `${record._pacient.ot}`.trim().length === 0) { 
-                if (record.zl_list.zap.z_sl.os_sluch !== "2") {
-                    error.status = true;
-                }
-            }
-            return error;
-        }
 
         function run(record) {
             const errors = [];
