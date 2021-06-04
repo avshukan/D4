4d13e776c39ff62d33f718afac17bb1ff84ae71a
Aleksey Lomov <zibloidix@gmail.com>
2021-06-04 11:31:43 +1100

    Fix error with random check.code

--- public/app/res/js/checkModelFactory.js
+++ public/app/res/js/checkModelFactory.js
@@ -7,29 +7,26 @@
         ]);
 
     function CheckModelFactory(){
-        const checkModel = {
-            "code": '' + Math.random().toString().slice(2, 10),
-            "code_mo": "0",
-            "year": "0",
-            "month": "0",
-            "nschet": "0",
-            "dschet": "",
-            "plat": "65001",
-            "summav": "0.00",
-            "coments": null,
-            "summap": null,
-            "sank_mek": null,
-            "sank_mee": null,
-            "sank_ekmp": null
-        }
-
         return {
             new: (lpuCode, type) => {
-                checkModel.code_mo = lpuCode;
-                checkModel.coments = type;
-                return checkModel;
+              return {
+                "code": '' + Math.random().toString().slice(2, 10),
+                "code_mo": "0",
+                "year": "0",
+                "month": "0",
+                "nschet": "0",
+                "dschet": "",
+                "plat": "65001",
+                "summav": "0.00",
+                "coments": null,
+                "summap": null,
+                "sank_mek": null,
+                "sank_mee": null,
+                "sank_ekmp": null,
+                "code_mo": lpuCode,
+                "coments": type
+              }
             }
         };
-
     }
 })(); 
